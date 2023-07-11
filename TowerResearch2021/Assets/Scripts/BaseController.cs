c69bdb97f4c51f88e0005bca34a286ad5845dbe8
JesseSchwartz25 <jesseschwartz25@gmail.com>
2023-07-11 15:41:37 -0400

    another one

--- TowerResearch2021/Assets/Scripts/BaseController.cs
+++ TowerResearch2021/Assets/Scripts/BaseController.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEditor.Animations;
 using UnityEngine.UI;
 
 public class BaseController : MonoBehaviour
 {
     private Vector3 rotate;
-    //public Animator baseAnimator;
     public Button spinClockwise;
     public Button spinCounter;
     public Button resetButton;
@@ -15,34 +13,40 @@ public class BaseController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //baseAnimator = GetComponent<Animator>();
         rotate = new Vector3(0, 50, 0);
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (spinClockwise.GetComponent<ButtonManager>().state == 2)
-        {
-            RotateClockwise();
-        }
+    // void Update()
 
-        if (spinCounter.GetComponent<ButtonManager>().state == 2)
-        {
-            RotateCounterClockwise();
-        }
+    // //! [07/23] Pretty sure all of this is obsolete
+    // //TODO: Verify obsolete
+    // {
+    //     if (spinClockwise.GetComponent<ButtonManager>().state == 2)
+    //     {
+    //        RotateClockwise();
+    //     }
 
-        if (spinCounter.GetComponent<ButtonManager>().newGrabberRelease())
-        {
-            spinCounter.GetComponent<ButtonManager>().buttonReset();
-            spinClockwise.GetComponent<ButtonManager>().buttonReset();
-            resetButton.GetComponent<ButtonManager>().buttonReset();
+    //     if (spinCounter.GetComponent<ButtonManager>().state == 2)
+    //     {
+    //        RotateCounterClockwise();
+    //     }
 
-        }
+    //     if (spinCounter.GetComponent<ButtonManager>().newGrabberRelease())
+    //     {
+    //        spinCounter.GetComponent<ButtonManager>().buttonReset();
+    //        spinClockwise.GetComponent<ButtonManager>().buttonReset();
+    //        resetButton.GetComponent<ButtonManager>().buttonReset();
 
-    }
+    //     }
 
+    // }
+
+    
+
+
+    //These functions are taken directly by the three buttons in the UI    
     public void RotateClockwise()
     {
 
@@ -59,6 +63,7 @@ public class BaseController : MonoBehaviour
     public void ResetRotation()
     {
         LeanTween.rotate(this.gameObject, new Vector3(0, 0, 0), 1.0f).setEaseInOutCubic();
-        //leantween is a library that makes some animations a little bit easier to do in code
+        //LeanTween is a library that makes some animations a little bit easier to do in code
+        //this one just resets the base to rotation 0 in a cubic manner in 1 second but it has lots of other cool applications in unity
     }
 }
